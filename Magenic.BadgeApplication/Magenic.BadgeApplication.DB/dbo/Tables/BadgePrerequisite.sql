28f174e49e6269daf5fc4b06b88a66b34c57569e
Kevin E. Ford <Bowman74@hotmail.com>
2013-12-01 17:56:12 -0500

    Initial setup of business logic

---
+++ Magenic.BadgeApplication/Magenic.BadgeApplication.DB/dbo/Tables/BadgePrerequisite.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[BadgePrerequisite] (
+    [PrerequisiteID]  INT IDENTITY (1, 1) NOT NULL,
+    [BadgeId]         INT NULL,
+    [RequiredBadgeId] INT NULL,
+    CONSTRAINT [PK_BadgePrerequisite] PRIMARY KEY CLUSTERED ([PrerequisiteID] ASC),
+    CONSTRAINT [FK_BadgePrerequisite_Badge] FOREIGN KEY ([BadgeId]) REFERENCES [dbo].[Badge] ([BadgeId]),
+    CONSTRAINT [FK_BadgePrerequisite_Badge1] FOREIGN KEY ([RequiredBadgeId]) REFERENCES [dbo].[Badge] ([BadgeId])
+);
+
