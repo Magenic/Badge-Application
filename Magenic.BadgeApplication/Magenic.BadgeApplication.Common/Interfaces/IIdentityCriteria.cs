7459fbce9f0fc795545f2c3f18ce5940745b6673
Kevin E. Ford <Bowman74@hotmail.com>
2013-12-07 23:35:02 -0500

    Added Entity Framework project and custom principal and identity objects.

---
+++ Magenic.BadgeApplication/Magenic.BadgeApplication.Common/Interfaces/IIdentityCriteria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magenic.BadgeApplication.Common.Interfaces
+{
+    /// <summary>
+    /// Specifies informaiton to use to fetch an Identity.
+    /// </summary>
+    public interface IIdentityCriteria
+    {
+        /// <summary>
+        /// The username for the identity.
+        /// </summary>
+        string UserName { get; set; }
+        /// <summary>
+        /// The unencrypted password.
+        /// </summary>
+        string Password { get; set; }
+    }
+}
