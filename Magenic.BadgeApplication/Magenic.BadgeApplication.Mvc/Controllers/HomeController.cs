d87f275225a3d8db81b2db9739db31db990bfeb3
jwendl <jwendl@hotmail.com>
2013-12-09 23:07:54 -0600

    Removing unused code.

--- Magenic.BadgeApplication/Magenic.BadgeApplication.Mvc/Controllers/HomeController.cs
+++ Magenic.BadgeApplication/Magenic.BadgeApplication.Mvc/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using Magenic.BadgeApplication.BusinessLogic.Badge;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Magenic.BadgeApplication.Controllers
 {
@@ -13,10 +12,8 @@ namespace Magenic.BadgeApplication.Controllers
         /// Handles the /Home/Index action.
         /// </summary>
         /// <returns></returns>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1804:RemoveUnusedLocals", MessageId = "badges")]
         public virtual ActionResult Index()
         {
-            var badges = BadgeEdit.GetBadgeEditById(1);
             return View();
         }
     }
