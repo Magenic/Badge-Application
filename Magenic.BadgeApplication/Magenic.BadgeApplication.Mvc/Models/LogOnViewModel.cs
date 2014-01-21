8d6a605628961aee9926667110114d5d0bea2473
jwendl <jwendl@hotmail.com>
2014-01-20 22:41:12 -0600

    Implemented security changes based on CSLA service locator

---
+++ Magenic.BadgeApplication/Magenic.BadgeApplication.Mvc/Models/LogOnViewModel.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Magenic.BadgeApplication.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class LogOnViewModel
+    {
+        /// <summary>
+        /// Gets or sets the name of the user.
+        /// </summary>
+        /// <value>
+        /// The name of the user.
+        /// </value>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>
+        /// The password.
+        /// </value>
+        public string Password { get; set; }
+    }
+}
