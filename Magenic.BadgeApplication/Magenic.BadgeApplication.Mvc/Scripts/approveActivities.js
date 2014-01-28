de76f0c26f2dd8b97ef342933d00130421356564
jwendl <jwendl@hotmail.com>
2014-01-27 23:20:31 -0600

    Finished approve activities functionality.

--- Magenic.BadgeApplication/Magenic.BadgeApplication.Mvc/Scripts/approveActivities.js
+++ Magenic.BadgeApplication/Magenic.BadgeApplication.Mvc/Scripts/approveActivities.js
@@ -1,1 +1,18 @@
-﻿
+﻿function refreshPreviousActivitiesList() {
+    var listUrl = $('#UrlToList').val();
+    $('#activitiesForApproval').load(listUrl);
+}
+
+function approveActivity(element, activityId) {
+    var approvalUrl = $('#UrlToApprove').val();
+    $.post(approvalUrl, { SubmissionId: activityId }, function (data) {
+        refreshPreviousActivitiesList();
+    });
+}
+
+function rejectActivity(element, activityId) {
+    var rejectionUrl = $('#UrlToReject').val();
+    $.post(rejectionUrl, { SubmissionId: activityId }, function (data) {
+        refreshPreviousActivitiesList();
+    });
+}
