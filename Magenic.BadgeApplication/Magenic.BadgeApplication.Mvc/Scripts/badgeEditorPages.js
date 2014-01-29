048be1bee752a04338573a39961faf22a9e00541
Justin Wendlandt <jwendl@hotmail.com>
2014-01-29 12:45:11 -0600

    Submitting changes before compile fixes.

--- Magenic.BadgeApplication/Magenic.BadgeApplication.Mvc/Scripts/badgeEditorPages.js
+++ Magenic.BadgeApplication/Magenic.BadgeApplication.Mvc/Scripts/badgeEditorPages.js
@@ -1,5 +1,11 @@
 ﻿$(document).ready(function () {
     $(':file').filestyle();
     $('.datepicker').datepicker();
+
+    $('.datepicker').each(function () {
+        var startingValue = $(this).val();
+        $(this).val(startingValue.substring(0, startingValue.indexOf(' ')));
+    });
+
     $('.selectpicker').selectpicker();
 });
