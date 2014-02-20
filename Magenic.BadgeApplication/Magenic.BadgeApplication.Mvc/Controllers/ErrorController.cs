756c7d683b7f1b28be641f38a9b165ef6eb0bae9
jwendl <jwendl@hotmail.com>
2014-02-19 20:35:32 -0600

    Fixing web.config to handle 403 errors with a better message.

---
+++ Magenic.BadgeApplication/Magenic.BadgeApplication.Mvc/Controllers/ErrorController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+
+namespace Magenic.BadgeApplication.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ErrorController
+        : Controller
+    {
+        /// <summary>
+        /// The default error page
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Default()
+        {
+            return this.View();
+        }
+
+        /// <summary>
+        /// Handles 404 error
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult NotFound()
+        {
+            return this.View();
+        }
+
+        /// <summary>
+        /// Handles 403 error
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AccessDenied()
+        {
+            return this.View();
+        }
+    }
+}
