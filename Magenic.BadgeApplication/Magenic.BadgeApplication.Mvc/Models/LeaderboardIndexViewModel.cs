11d73bad2c5d34c9706fda938d959e80538df2fc
jwendl <jwendl@hotmail.com>
2014-04-16 19:26:24 -0500

    Added a section to the web config for profile photos

--- Magenic.BadgeApplication/Magenic.BadgeApplication.Mvc/Models/LeaderboardIndexViewModel.cs
+++ Magenic.BadgeApplication/Magenic.BadgeApplication.Mvc/Models/LeaderboardIndexViewModel.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
+﻿
 namespace Magenic.BadgeApplication.Models
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class LeaderboardIndexViewModel
     {
     }
