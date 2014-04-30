5d78424fa3ca04fa7ee8e50b43b8135ce59a88ed
jwendl <jwendl@hotmail.com>
2014-04-30 01:30:49 -0500

    Fixing issues with WP 8 and IE 10.

---
+++ Magenic.BadgeApplication/Magenic.BadgeApplication.Mvc/Scripts/global.js
@@ -0,0 +1,10 @@
+﻿// Fix for IE10 WP 8 bug http://getbootstrap.com/getting-started/#support-ie10-width
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement('style')
+    msViewportStyle.appendChild(
+      document.createTextNode(
+        '@-ms-viewport{width:auto!important}'
+      )
+    )
+    document.querySelector('head').appendChild(msViewportStyle)
+}
