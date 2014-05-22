970f785c4bd120fec69f0edba1a0e42bd3b28d1b
Justin Wendlandt <jwendl@hotmail.com>
2014-05-22 16:36:44 -0500

    Initial work on the file loader for activities.

---
+++ Magenic.BadgeApplication/Magenic.BadgeApplication.Activity.Console/Starter.cs
@@ -0,0 +1,34 @@
+﻿using Magenic.BadgeApplication.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magenic.BadgeApplication.Activity.Console
+{
+    public class Starter
+    {
+        public async static Task Start(bool startAsConsole)
+        {
+                    try
+            {
+                AutofacBootstrapper.Init();
+
+                //var adProcessor = new Processor.FileLoader();
+                //if (startAsConsole)
+                //{
+                //    await adProcessor.StartAsync();
+                //}
+                //else
+                //{
+                //    Task.Run(() => adProcessor.StartAsync());
+                //}
+            }
+            catch (Exception ex)
+            {
+                //Logger.Fatal<Program>(ex.Message, ex);
+            }
+        }
+    }
+}
