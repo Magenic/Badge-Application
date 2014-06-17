e2fae398c346978295db369ab863ee97511b4315
jwendl <jwendl@hotmail.com>
2014-06-16 21:47:03 -0500

    Revert "Revert "Finishing up CSLA work for new functionality.""
    This reverts commit 1254f229ba0ce7c57470270dce3dcc80f6bfaa87.

---
+++ Magenic.BadgeApplication/Magenic.BadgeApplication.DataAccess.EF/BadgeAwardEditCollectionDAL.cs
@@ -0,0 +1,41 @@
+﻿using Magenic.BadgeApplication.Common.DTO;
+using Magenic.BadgeApplication.Common.Interfaces;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Magenic.BadgeApplication.DataAccess.EF
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BadgeAwardEditCollectionDAL
+        : IBadgeAwardEditCollectionDAL
+    {
+        /// <summary>
+        /// Gets all badge awards for user asynchronous.
+        /// </summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <returns></returns>
+        public async Task<IEnumerable<BadgeAwardEditDTO>> GetAllBadgeAwardsForUserAsync(string userName)
+        {
+            using (var ctx = new Entities())
+            {
+                ctx.Database.Connection.Open();
+                var badgeAwardList = await (from ba in ctx.BadgeAwards
+                                            select new BadgeAwardEditDTO
+                                            {
+                                                Id = ba.BadgeAwardId,
+                                                EmployeeId = ba.Employee.EmployeeId,
+                                                BadgeId = ba.BadgeId,
+                                                BadgeName = ba.Badge.BadgeName,
+                                                AwardDate = ba.AwardDate,
+                                                AwardAmount = ba.AwardAmount,
+                                            }).ToArrayAsync();
+
+                return badgeAwardList;
+            }
+        }
+    }
+}
