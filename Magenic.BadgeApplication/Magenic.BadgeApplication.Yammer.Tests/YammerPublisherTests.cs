3e3dc727dd633212d149045351b29c740f1df7fc
BillZiss <billzi@magenic.com>
2017-03-24 10:08:53 -0400

    #251 - Adding folder for unit tests for fix of issue #251

---
+++ Magenic.BadgeApplication/Magenic.BadgeApplication.Yammer.Tests/YammerPublisherTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Magenic.BadgeApplication.Yammer;
+using Magenic.BadgeApplication.Common.DTO;
+
+namespace Magenic.BadgeApplication.Yammer.Tests
+{
+	[TestClass]
+	public class YammerPublisherTests
+	{
+		[TestMethod]
+		public void PublishHarness()
+		{
+			var dto = new EarnedBadgeItemDTO
+			{
+				EmployeeADName = "billzi",
+				Name = "testBadgeName2",
+				ImagePath = "testImagePath2",
+				Tagline = "testTagLine2"
+			};
+
+			YammerPublisher yp = new YammerPublisher();
+
+			yp.Publish(dto);
+			
+
+		}
+	}
+}
