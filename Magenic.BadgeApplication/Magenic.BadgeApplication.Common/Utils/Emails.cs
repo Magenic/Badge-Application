ad8235aeef89cedbc2e3145e108184cad930fa7e
NormanG <normang@magenic.com>
2020-03-22 23:01:14 -0500

    bypass certificate error

--- Magenic.BadgeApplication/Magenic.BadgeApplication.Common/Utils/Emails.cs
+++ Magenic.BadgeApplication/Magenic.BadgeApplication.Common/Utils/Emails.cs
@@ -62,6 +62,15 @@ namespace Magenic.BadgeApplication.Common.Utils
                     mailMessage.From = fromEmail;
                 }
 
+                //Add this line to bypass the certificate validation
+                System.Net.ServicePointManager.ServerCertificateValidationCallback = delegate (object s,
+                        System.Security.Cryptography.X509Certificates.X509Certificate certificate,
+                        System.Security.Cryptography.X509Certificates.X509Chain chain,
+                        System.Net.Security.SslPolicyErrors sslPolicyErrors)
+                {
+                    return true;
+                };
+
                 using (var smtpClient = new SmtpClient())
                 {
                     smtpClient.Send(mailMessage);
